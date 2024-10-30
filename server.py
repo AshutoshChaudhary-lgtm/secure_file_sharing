e4b087174afb87612c94b41f9149d091fb811329
CursedCoder02 <ch.ashutosh18@gmail.com>
2024-10-30 23:25:51 +0530

    removed path traversal vulnerability

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, safe_join, abort
 import os
 
 app = Flask(__name__)
@@ -7,12 +7,28 @@ DOWNLOAD_FOLDER = '/path/to/download'
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
     file = request.files['file']
-    file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    if file.filename == '':
+        return 'No selected file', 400
+
+    filename = os.path.basename(file.filename)
+    safe_path = safe_join(UPLOAD_FOLDER, filename)
+
+    if not safe_path.startswith(os.path.abspath(UPLOAD_FOLDER)):
+        return 'Invalid file path', 400
+
+    file.save(safe_path)
     return 'File uploaded successfully'
 
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
+    safe_path = safe_join(DOWNLOAD_FOLDER, filename)
+
+    if not safe_path.startswith(os.path.abspath(DOWNLOAD_FOLDER)):
+        return abort(404)
+
     return send_from_directory(DOWNLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
