18c520848494ba165e9880860aafac9c9ffb665e
CursedCoder02 <ch.ashutosh18@gmail.com>
2024-11-01 22:38:17 +0530

    secret key reference to env

--- settings.py
+++ settings.py
@@ -1,11 +1,16 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key'
+# Load environment variables
+load_dotenv()
+
+# Get SECRET_KEY from environment variable
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
