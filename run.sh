36cd138b5a653aeca2c446a84e2c9c3e3a5e2b67
CursedCoder02 <ch.ashutosh18@gmail.com>
2024-11-01 15:51:26 +0530

    1

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
 # Create and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
-
 # Install dependencies
 pip install -r requirements.txt
-
 # Initialize the database
 python manage.py makemigrations
 python manage.py migrate
-
 # Run the Django development server
 python manage.py runserver
