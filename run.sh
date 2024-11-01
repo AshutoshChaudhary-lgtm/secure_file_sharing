1f824b58b3a6b2c39e39dc73d6a6fad6d6db3abf
CursedCoder02 <ch.ashutosh18@gmail.com>
2024-11-02 00:07:59 +0530

    use bash idiot

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ source venv/bin/activate
 # Install dependencies
 pip install -r requirements.txt
 # Initialize the database
-python manage.py makemigrations
+python manage.py makemigrations app
 python manage.py migrate
 # Run the Django development server on all interfaces
 python manage.py runserver 0.0.0.0:8000
