f31fef4f355ec125708e94e01dc803e84a24c2c9
AshutoshChaudhary-lgtm <ch.ashutosh18@gmail.com>
2024-11-09 19:01:09 +0530

    python3.11

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
-# run.sh
 #!/bin/bash
+
 # Create and activate virtual environment
-python3 -m venv venv
+python3.11 -m venv venv
 source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
+
 # Initialize the database
 python manage.py makemigrations app
 python manage.py migrate
+
 # Run the Django development server on all interfaces
 python manage.py runserver 0.0.0.0:8000
