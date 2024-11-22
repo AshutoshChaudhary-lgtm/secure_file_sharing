56a520ea91d56a8bd7157aab4eb01b3f8f3676e5
AshutoshChaudhary <ch.ashutosh18@gmail.com>
2024-11-22 19:33:25 +0530

    Remove run.py script as it is no longer needed for project execution

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import django
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secure_file_sharing.settings')
-    django.setup()
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(['manage.py', 'runserver'])
